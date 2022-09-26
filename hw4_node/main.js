68de80950865e649ac2a601dcbf65b25af687f24
Yuloi <viegocat@yandex.ru>
2022-09-26 14:47:28 +0300

    added delete functionality

--- hw4_node/main.js
+++ hw4_node/main.js
@@ -12,18 +12,23 @@ function createFile(fileName) {
     fse.ensureFileSync(fileName);
 };
 function moveObject(object1,object2) { // src <String> dest <String> Note: When src is a file, dest must be a file and when src is a directory, dest must be a directory.
-    fse.moveSync(object1,object2, { overwrite: true })
+    fse.moveSync(object1,object2, { overwrite: true });
+};
+function copyObject(obj1,obj2) {
+    fse.copySync(obj1,obj2);
+};
+function emptyFolder(dir) { //empty folder, if folder doesn't exist create empty folder 
+    fse.emptyDirSync(dir);
 }
-
-
-
-
-//fse.emptyDirSync(firstFolder);
-//fse.removeSync(firstFolder);
+function removeObject(path) { // remove dir or file, if path doesnt exist do nothing
+    fse.removeSync(path);
+}
 
 createFolder(firstFolder);
 createFile(firstFolder + file);
 createFolder(secondFolder);
 moveObject(firstFolder + file, secondFolder + file);
 createFolder(thirdFolder);
-
+copyObject(secondFolder + file, thirdFolder + file);
+emptyFolder(secondFolder);
+removeObject(firstFolder);
