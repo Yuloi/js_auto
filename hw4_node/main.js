c5c3d8a40db2bb3696b84920d4e934e362c0754e
Yuloi <viegocat@yandex.ru>
2022-09-26 14:06:33 +0300

    added functionality for creating folder and file

--- hw4_node/main.js
+++ hw4_node/main.js
@@ -1,2 +1,25 @@
 const fse =  require('fs-extra');
-console.log(fse);
+
+let firstFolder = './firstFolder';
+let secondFolder = './secondFolder';
+let thirdFolder = './thirdFolfer';
+let file = './firstFolder/newfile.txt'
+
+function createFolder(folderName) { // function for creating folders
+    fse.ensureDirSync(folderName);
+};
+function createFile(fileName) {
+    fse.ensureFileSync(fileName);
+};
+
+
+//fse.copyFileSync
+
+
+//fse.emptyDirSync(firstFolder);
+//fse.removeSync(firstFolder);
+
+createFolder(firstFolder);
+createFile(file);
+createFolder(secondFolder);
+createFolder(thirdFolder);
