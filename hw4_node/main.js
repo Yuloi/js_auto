b31f07e9f0cf21f03b46379ddd75922c12480202
Yuloi <viegocat@yandex.ru>
2022-09-26 14:21:46 +0300

    added move functionality

--- hw4_node/main.js
+++ hw4_node/main.js
@@ -3,7 +3,7 @@ const fse =  require('fs-extra');
 let firstFolder = './firstFolder';
 let secondFolder = './secondFolder';
 let thirdFolder = './thirdFolfer';
-let file = './firstFolder/newfile.txt'
+let file = '/newfile.txt';
 
 function createFolder(folderName) { // function for creating folders
     fse.ensureDirSync(folderName);
@@ -11,15 +11,19 @@ function createFolder(folderName) { // function for creating folders
 function createFile(fileName) {
     fse.ensureFileSync(fileName);
 };
+function moveObject(object1,object2) { // src <String> dest <String> Note: When src is a file, dest must be a file and when src is a directory, dest must be a directory.
+    fse.moveSync(object1,object2, { overwrite: true })
+}
 
 
-//fse.copyFileSync
 
 
 //fse.emptyDirSync(firstFolder);
 //fse.removeSync(firstFolder);
 
 createFolder(firstFolder);
-createFile(file);
+createFile(firstFolder + file);
 createFolder(secondFolder);
+moveObject(firstFolder + file, secondFolder + file);
 createFolder(thirdFolder);
+
