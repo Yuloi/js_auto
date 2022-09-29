3da202a7dd55c423b384a5ab5bdef952c346596f
Yuloi <viegocat@yandex.ru>
2022-09-29 15:43:13 +0300

    added addition functionality woth error fallback

--- hw5/main.js
+++ hw5/main.js
@@ -2,19 +2,26 @@ function addition (a, b) {
     let typeA = typeof a;
     switch (typeA){
         case 'string':
-            b = new String(b);
+            b =  String(b);
+            console.log(b)
             console.log (`Addition for ${a} and ${b} is ${a+b}`);
             break;
         case 'number':
-            b = new Number(b);
-            console.log (`Addition for ${a} and ${b} is ${a+b}`);  //add caase for Nan
+            b =  Number(b);
+            if (isNaN(b) === true){
+                console.log(`Try again. Second value can't be converted to a number`);
+            }else{
+                console.log (`Addition for ${a} and ${b} is ${a+b}`);
+            }
             break;
         case 'boolean':
-            b = new Number(Newb);
-            if (Newb === NaN){
-                console.log(`${b}`)
+            b = new Number(b);
+            if (isNaN(b) === true) {
+                console.log(`Try again. Second value can't be converted to a number`);
+            }else{
+                console.log (`Addition for ${a} and ${b} is ${a+b}`);
             }
-            console.log (`Addition for ${a} and ${b} is ${a+b}`);
+            break;
     };
 };
-addition(true, '123');
+addition(0, false);
