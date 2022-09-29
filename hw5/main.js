a2ae489f0669edbceab8926d2dc4fa5462a7754d
Yuloi <viegocat@yandex.ru>
2022-09-29 16:50:49 +0300

    added divide functionality

--- hw5/main.js
+++ hw5/main.js
@@ -2,54 +2,68 @@
 This programm works only with boolean, string or number type
 issues: a === ''
 */
-function addition (a, b) {
+function addition(a, b) {
     let numA = Number(a);
     let typeA;
-    if (isNaN(numA) === true){
-        typeA =  typeof a;
-    }else{
+    if (isNaN(numA) === true) {
+        typeA = typeof a;
+    } else {
         a = numA;
         typeA = typeof a;
     };
-    switch (typeA){
+    switch (typeA) {
         case 'string':
-            b =  String(b);
-            console.log (`Addition for ${a} and ${b} is ${a+b}`);
+            b = String(b);
+            console.log(`Addition for ${a} and ${b} is ${a + b}`);
             break;
         case 'number':
-            b =  Number(b);
-            if (isNaN(b) === true){
+            b = Number(b);
+            if (isNaN(b) === true) {
                 console.log(`Try again. Second value can't be converted to a number`);
-            }else{
-                console.log (`Addition for ${a} and ${b} is ${a+b}`);
+            } else {
+                console.log(`Addition for ${a} and ${b} is ${a + b}`);
             }
             break;
         case 'boolean':
             b = new Number(b);
             if (isNaN(b) === true) {
                 console.log(`Try again. Second value can't be converted to a number`);
-            }else{
-                console.log (`Addition for ${a} and ${b} is ${a+b}`);
+            } else {
+                console.log(`Addition for ${a} and ${b} is ${a + b}`);
             }
             break;
     };
 };
 
-function multiply (a, b){
+function multiply(a, b) {
     let numA = Number(a);
     let numB = Number(b);
-    if (isNaN(numA) === true || isNaN(numB) === true){
+    if (isNaN(numA) === true || isNaN(numB) === true) {
         //typeA = typeof a;
         console.log(`Multiplying can't be performed, values can't be converted into numbers`);
 
-    }else{
+    } else {
         a = numA
         b = numB
-        console.log (`Multiplying for ${a} and ${b} is ${a * b}`)
+        console.log(`Multiplying for ${a} and ${b} is ${a * b}`)
     };
 
 }
+function divide(a, b) {
+    let numA = Number(a);
+    let numB = Number(b);
+    if (isNaN(numA) === true || isNaN(numB) === true) {
+        //typeA = typeof a;
+        console.log(`Division can't be performed, values can't be converted into numbers`);
 
-//addition('true', ''); // fix issue where a === '' + false 
-//multiply('44', 6)
+    } else {
+        a = numA
+        b = numB
+        console.log(`Division for ${a} and ${b} is ${a / b}`)
+    };
+
+}
+addition(false, '45'); // fix issue where a === '' + false 
+multiply('45', false);
+divide('45', false);
 
