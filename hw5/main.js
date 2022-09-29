ca563d05bc947e2148a4846609bfeb4c032595cb
Yuloi <viegocat@yandex.ru>
2022-09-29 15:20:18 +0300

    added main logic for addition functionality

---
+++ hw5/main.js
@@ -0,0 +1,20 @@
+function addition (a, b) {
+    let typeA = typeof a;
+    switch (typeA){
+        case 'string':
+            b = new String(b);
+            console.log (`Addition for ${a} and ${b} is ${a+b}`);
+            break;
+        case 'number':
+            b = new Number(b);
+            console.log (`Addition for ${a} and ${b} is ${a+b}`);  //add caase for Nan
+            break;
+        case 'boolean':
+            b = new Number(b);
+            if (b === NaN){
+                console.log(`${b}`)
+            }
+            console.log (`Addition for ${a} and ${b} is ${a+b}`);
+    };
+};
+addition(true, '123');
