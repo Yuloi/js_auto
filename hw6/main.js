7817489bbb6945478c99e984d2e2b2bfef97c92c
Yuloi <viegocat@yandex.ru>
2022-10-02 19:55:48 +0300

    added function for task 2

--- hw6/main.js
+++ hw6/main.js
@@ -1,5 +1,9 @@
-function stringToArray(stringValue) {
-    let replaceValue = stringValue.replace(/ /g, '');
+/*
+To do: 
+Update task 1 for calculating sum using method not + 
+*/
+function stringToArray(value) {
+    let replaceValue = value.replace(/ /g, '');
     let arr = replaceValue.split('');
     let i = 0;
     for (i; i < arr.length; i++){
@@ -8,8 +12,8 @@ function stringToArray(stringValue) {
     return arr;
     
 };
-function sum (stringValue) {
-    let arr = stringToArray(stringValue);
+function sum (value) {
+    let arr = stringToArray(value);
     const sum1 = arr[0] + arr[1] + arr[2];
     const sum2 = arr[3] + arr[4] + arr[5];
     if (sum1 === sum2){
@@ -18,4 +22,17 @@ function sum (stringValue) {
         console.log('Нет');
     }
 };
-sum(' 333 801 ')
+// task 2
+function divisionTill50 (n){
+    let num = 0;
+    for (n; n >= 50;){
+        num++;
+        n = n/2;
+    }
+    console.log(`Last value for division is ${n}, it will be done for ${num} interactions`);
+}
+
+
+
+module.exports.sum = sum;
+module.exports.divisionTill50 = divisionTill50;
