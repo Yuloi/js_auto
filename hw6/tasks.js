1d5bb611cad120fe6adb12e84c5d63ea79782c55
Yuloi <viegocat@yandex.ru>
2022-10-02 20:44:57 +0300

    added task 3

--- hw6/tasks.js
+++ hw6/tasks.js
@@ -1,8 +1,8 @@
-const { sum } = require('./main');
-const {divisionTill50} = require('./main');
+const { sum, divisionTill50, averageValue } = require('./main');
 
 // task 1
 sum(' 303 41 1');
 // task 2
 divisionTill50(1000);
 // task 3
+averageValue([12,15,20,35,59,79]);
