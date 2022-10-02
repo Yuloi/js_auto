766727c79c58895fc49adb0efcdfdeba6f717268
Yuloi <viegocat@yandex.ru>
2022-10-02 20:44:33 +0300

    created function for task 3

--- hw6/main.js
+++ hw6/main.js
@@ -1,6 +1,7 @@
 /*
 To do: 
 Update task 1 for calculating sum using method not + 
+task 1
 */
 function stringToArray(value) {
     let replaceValue = value.replace(/ /g, '');
@@ -13,7 +14,7 @@ function stringToArray(value) {
     
 };
 function sum (value) {
-    let arr = stringToArray(value);
+    const arr = stringToArray(value);
     const sum1 = arr[0] + arr[1] + arr[2];
     const sum2 = arr[3] + arr[4] + arr[5];
     if (sum1 === sum2){
@@ -31,8 +32,15 @@ function divisionTill50 (n){
     }
     console.log(`Last value for division is ${n}, it will be done for ${num} interactions`);
 }
-
-
+// task 3
+function averageValue (n){
+    const arr = n;
+    const newArr = arr.reduce(function(previousValue, currentValue){
+        return previousValue + currentValue
+    });
+    console.log(`Average value for ${arr} is ${newArr/arr.length}`);
+}
 
 module.exports.sum = sum;
 module.exports.divisionTill50 = divisionTill50;
+module.exports.averageValue = averageValue;
