e47ceb158674733b29b124241e665fb51235cd81
Yuloi <viegocat@yandex.ru>
2022-10-09 14:29:39 +0300

    added first task

---
+++ hw8/main.js
@@ -0,0 +1,8 @@
+// TASK 1
+function reverseArray (arr){ // note: this function will mutate original array
+    return arr.reverse();
+};
+// TASK 2
+function test (){}
+
+module.exports.reverseArray = reverseArray;
---
+++ hw8/tasks.js
@@ -0,0 +1,4 @@
+const {reverseArray} = require('./main')
+// TASK 1
+arr = [1,2,3,4,5,6]
+console.log(reverseArray(arr));
