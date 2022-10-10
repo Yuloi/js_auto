f67663f44d9d665328cfdd5044942a812ceaed2b
Yuloi <viegocat@yandex.ru>
2022-10-10 14:33:02 +0300

    added task 3

--- hw8/main.js
+++ hw8/main.js
@@ -7,7 +7,21 @@ function findMaxInArray (arr){
     let sortedArr = arr.sort((a,b)=> {return b - a})
     return sortedArr[0]
 }
-// 
+// TASK 3
+const fibArray = [];
+let F0 = 0;
+let F1 = 1;
+
+function fibonacciArray(N,M){
+    for (i = 1; i <= N+M; i++){
+        let Fn = F0 + F1;
+        fibArray.push(Fn)
+        F0 = F1;
+        F1 = Fn;
+    }; return fibArray.slice(N, N+M)
+
+};
 
 module.exports.reverseArray = reverseArray;
 module.exports.findMaxInArray = findMaxInArray;
+module.exports.fibonacciArray = fibonacciArray;
--- hw8/tasks.js
+++ hw8/tasks.js
@@ -1,8 +1,12 @@
-const {reverseArray, findMaxInArray} = require('./main')
+const {reverseArray, findMaxInArray, fibonacciArray} = require('./main')
 
-arr = [1,2,3,4,5,6]
-arr2 = [3,67,15,80,23,35,11,5,124, 9]
+const arr = [1,2,3,4,5,6]
+const arr2 = [3,67,15,80,23,35,11,5,124, 9]
+let N = 4;
+let M = 6;
 // TASK 1
 console.log(reverseArray(arr));
 // TASK 2
-console.log(findMaxInArray(arr2))
+console.log(findMaxInArray(arr2));
+// TASK 3
+console.log(fibonacciArray(N,M));
