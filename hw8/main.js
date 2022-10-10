b3d6e5876667723afe8bed00611bebd7fa57a4d3
Yuloi <viegocat@yandex.ru>
2022-10-10 17:42:33 +0300

    added task 4

--- hw8/main.js
+++ hw8/main.js
@@ -1,27 +1,68 @@
-// TASK 1
-function reverseArray (arr){ // note: this function will mutate original array
-    return arr.reverse();
-};
-// TASK 2
-function findMaxInArray (arr){
-    let sortedArr = arr.sort((a,b)=> {return b - a})
-    return sortedArr[0]
-}
-// TASK 3
 const fibArray = [];
 let F0 = 0;
 let F1 = 1;
+const SUM_4 = {
+    sumValues: 0,
+    sumPositionsValues: 0
+};
 
-function fibonacciArray(N,M){
-    for (i = 1; i <= N+M; i++){
+// TASK 1
+function reverseArray(arr) { // note: this function will mutate original array
+    return arr.reverse();
+};
+// TASK 2
+function findMaxInArray(arr) {
+    let sortedArr = arr.sort((a, b) => { return b - a })
+    return sortedArr[0]
+}
+// TASK 3
+function fibonacciArray(N, M) {
+    for (i = 1; i <= N + M; i++) {
         let Fn = F0 + F1;
         fibArray.push(Fn)
         F0 = F1;
         F1 = Fn;
-    }; return fibArray.slice(N, N+M)
+    }; return fibArray.slice(N, N + M)
 
 };
+// TASK 4
+function convertingValiestoArray(a, b) {
+    a = String(a);
+    b = String(b);
+    arrA = Array.from(a);
+    arrB = Array.from(b);
+    return { firstNumber: arrA, secondNumber: arrB }
+};
+function checkValueAndPosition(a, b) {
+    const result = convertingValiestoArray(a, b);
+    const number1 = result.firstNumber;
+    const number2 = result.secondNumber;
+    for (i = 0; i < number1.length; i++) {
+        if (number1[i] === number2[i]) {
+            SUM_4.sumPositionsValues = SUM_4.sumPositionsValues + 1;
+        } else {
+        };
+    }; return `Amount of numbers with the same position and value: ${SUM_4.sumPositionsValues}`
+};
+function checkValuesinArray (a, b){
+    const result = convertingValiestoArray(a, b);
+    const number1 = result.firstNumber;
+    const number2 = result.secondNumber;
+    for (i = 0; i < number1.length; i++){
+        let a = number2[i]
+        if (number1.includes(a)){
+            SUM_4.sumValues = SUM_4.sumValues + 1;
+        }else{
+        };
+    }return `Amount of numbers with the same value: ${SUM_4.sumValues}`
+};
+function compareValuesTogether (a, b){
+    console.log(checkValueAndPosition(a, b));
+    console.log(checkValuesinArray (a, b));
+}
+
 
 module.exports.reverseArray = reverseArray;
 module.exports.findMaxInArray = findMaxInArray;
 module.exports.fibonacciArray = fibonacciArray;
+module.exports.compareValuesTogether = compareValuesTogether;
--- hw8/tasks.js
+++ hw8/tasks.js
@@ -1,4 +1,8 @@
-const {reverseArray, findMaxInArray, fibonacciArray} = require('./main')
+const {reverseArray, 
+    findMaxInArray, 
+    fibonacciArray,
+    compareValuesTogether
+} = require('./main')
 
 const arr = [1,2,3,4,5,6]
 const arr2 = [3,67,15,80,23,35,11,5,124, 9]
@@ -10,3 +14,5 @@ console.log(reverseArray(arr));
 console.log(findMaxInArray(arr2));
 // TASK 3
 console.log(fibonacciArray(N,M));
+// TASK 4
+compareValuesTogether(3487,3794)
