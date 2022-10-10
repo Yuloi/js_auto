c237684fc7dbd1eaeee5c94a70c649cd44b1eb48
Yuloi <viegocat@yandex.ru>
2022-10-10 12:28:59 +0300

    added task 2

--- hw8/main.js
+++ hw8/main.js
@@ -3,6 +3,11 @@ function reverseArray (arr){ // note: this function will mutate original array
     return arr.reverse();
 };
 // TASK 2
-function test (){}
+function findMaxInArray (arr){
+    let sortedArr = arr.sort((a,b)=> {return b - a})
+    return sortedArr[0]
+}
+// 
 
 module.exports.reverseArray = reverseArray;
+module.exports.findMaxInArray = findMaxInArray;
--- hw8/tasks.js
+++ hw8/tasks.js
@@ -1,4 +1,8 @@
-const {reverseArray} = require('./main')
-// TASK 1
+const {reverseArray, findMaxInArray} = require('./main')
+
 arr = [1,2,3,4,5,6]
+arr2 = [3,67,15,80,23,35,11,5,124, 9]
+// TASK 1
 console.log(reverseArray(arr));
+// TASK 2
+console.log(findMaxInArray(arr2))
