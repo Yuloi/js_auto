0df0cadf56247184a5f4274c92654eb79b456e71
Yuloi <viegocat@yandex.ru>
2022-10-18 14:17:46 +0200

    added task 3

--- hw9/main.js
+++ hw9/main.js
@@ -1,28 +1,41 @@
 const getRandomNumber = (min,max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-// TASK 1
-const p1 = new Promise(function(resolve,reject){
-    setTimeout(() => resolve('1'), getRandomNumber(1,5) * 1000)
-}).then(value => value);
-
-const p2 = new Promise((resolve,reject)=>{
-    setTimeout(()=> resolve('2'), getRandomNumber(1,5) * 1000)
-}).then(value => value);
-
-const p3 = new Promise((resolve,reject)=>{
-    setTimeout(()=> resolve('3'), getRandomNumber(1,5) * 1000)
-}).then(value => value);
-
-Promise.race([p1,p2,p3]).then(value => console.log(value))
-
-// TASK 2
-function getNum(){
-    return new Promise(function(resolve,reject){
-        setTimeout(() => resolve(getRandomNumber(1,5)), 3000)
+function getPromisrandomNumber(start,finish,time){
+    return new Promise((resolve,reject)=> {
+        setTimeout(()=> resolve(getRandomNumber(start,finish)),time);
     }).then(value => value)
 };
+
+function returnPromisWithValue(number, start,finish, second = 1000){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => resolve(number), getRandomNumber(start,finish) * second)
+    }).then(value => value)
+}
+
+// TASK 1
+const p1 = returnPromisWithValue(1,1,5);
+const p2 = returnPromisWithValue(2,1,5);
+const p3 = returnPromisWithValue(3,1,5);
+
+Promise.race([p1,p2,p3]).then(value => console.log(value)) //promise race
+
+// TASK 2
+const getNum = getPromisrandomNumber(1,5,3000)
 async function getSquarOfNum(){
-    const num = await getNum();
+    const num = await getNum;
     console.log(`square of number ${num} is equal to ${num*num}`)
 };
 getSquarOfNum()
+
+// TASK 3
+
+
+const getNum1 = getPromisrandomNumber(1,5,3000);
+const getNum2 = getPromisrandomNumber(6,10,5000)
+
+async function getSumOf2Numbers(){
+    let num1 = await getNum1;
+    let num2 = await getNum2;
+    console.log(`Sum of ${num1} and ${num2} is equal to ${num1 + num2}`)
+}
+getSumOf2Numbers()
