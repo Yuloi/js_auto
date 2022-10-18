6e7258d8dd02ea40c55b6541dcdf0a9674b5f84e
Yuloi <viegocat@yandex.ru>
2022-10-18 14:17:46 +0200

    added task 3

--- hw9/main.js
+++ hw9/main.js
@@ -13,7 +13,7 @@ const p3 = new Promise((resolve,reject)=>{
     setTimeout(()=> resolve('3'), getRandomNumber(1,5) * 1000)
 }).then(value => value);
 
-Promise.race([p1,p2,p3]).then(value => console.log(value))
+Promise.race([p1,p2,p3]).then(value => console.log(value)) //promise race
 
 // TASK 2
 function getNum(){
@@ -26,3 +26,22 @@ async function getSquarOfNum(){
     console.log(`square of number ${num} is equal to ${num*num}`)
 };
 getSquarOfNum()
+
+// TASK 3
+function getNum1(){
+    return new Promise((resolve,reject)=> {
+        setTimeout(()=> resolve(getRandomNumber(1,5)),3000);
+    }).then(value => value)
+};
+function getNum2(){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => resolve(getRandomNumber(6,10)),5000);
+    }).then(value => value)
+};
+
+async function getSumOf2Numbers(){
+    let num1 = await getNum1();
+    let num2 = await getNum2();
+    console.log(`Sum of ${num1} and ${num2} is equal to ${num1 + num2}`)
+}
+getSumOf2Numbers()
