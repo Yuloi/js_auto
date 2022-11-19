8b3e227eccabec6c14d66262c4f5cd1bc0a79cd2
Yuloi <viegocat@yandex.ru>
2022-11-19 12:24:24 +0100

    added 2 test

---
+++ hw17/test/test.js
@@ -0,0 +1,25 @@
+const {Builder, By} = require('selenium-webdriver');
+/* const chrome = require('selenium-webdriver/chrome');
+const service = new chrome.ServiceBuilder('/usr/local/bin/chromedriver'); */
+const chai = require('chai');
+const expect = chai.expect
+
+describe('Checking title for pages -  Main and Chrome Extensions', function(){
+    let driver;
+    beforeEach(async function(){
+        driver = await new Builder().forBrowser('chrome').build();
+    })
+    after(async function(){
+        await driver.quit()
+    })
+
+    it('Main page title is changing after swithing pages', async function (){
+        await driver.get('https://chromedriver.chromium.org/home')
+        let homePageTitle = await driver.getTitle();
+        expect(homePageTitle).to.equal('ChromeDriver - WebDriver for Chrome');
+        let chromeExtentionButton = await driver.findElement(By.css('.jgXgSe.HlqNPb[href="/extensions"]'));
+        await chromeExtentionButton.click()
+        let extentionPageTitle = await driver.getTitle()
+        expect(extentionPageTitle).to.equal('ChromeDriver - WebDriver for Chrome - Chrome Extensions');
+    })
+})
